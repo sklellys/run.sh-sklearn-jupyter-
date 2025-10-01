e8c7c24cb97e7b5ac5d6cb103486efba59f761ee
sklellys <kamil.stanislaw.kolacz@gmail.com>
2025-10-01 19:40:39 +0000

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Exit immediately if a command fails
 set -e
-
-echo "Updating package list..."
 sudo apt update -y
-
-echo "Installing scikit-learn..."
 sudo apt install -y python3-sklearn python3-sklearn-lib python-sklearn-doc
-
-echo "Installing Jupyter Notebook..."
-sudo apt install -y jupyter-notebook
+sudo apt install jupyter-notebook
 
