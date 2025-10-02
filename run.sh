32b9ece47ac4f632a3d740b26581477998f429be
sklellys <kamil.stanislaw.kolacz@gmail.com>
2025-10-02 10:54:17 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ sudo apt install -y python3-sklearn python3-sklearn-lib python-sklearn-doc
 sudo apt install jupyter-notebook
 sudo apt-get install python3-pip
 sudo apt update
+sudo apt install python3-numpy
 sudo apt install python3-pandas
